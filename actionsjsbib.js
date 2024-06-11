e061c4ffbb2c5568a48b4e7d8f7a8c29e2732f4f
Rocas66 <66.castro@gmail.com>
2024-06-10 22:11:13 -0500

    HTML v0.2

--- actionsjsbib.js
+++ actionsjsbib.js
@@ -1,39 +1,32 @@
 
 function buscar2() 
-{	let bib = [{ISBN:"RRYY",titulo:"CIEN AÑOS",autor:"GABO"},{ISBN:"RRYY2",titulo:"CIEN AÑOS2",autor:"GABO"},{ISBN:"RRYY3",titulo:"CIEN AÑOS3",autor:"GABO"}];
-	alert("DATOS: " + bib[0].ISBN +" "+bib[0].titulo+" "+ bib[0].autor)	;
-	//elemento = document.getElementById("cont1");
-	//elemento.value = "Hola campeon";
-	//elementos = document.getElementsByClassName(".cont1");
-	//for (let x=0;x < elementos.length ; x++) {
-	//elementos.innerHTML = "MILLOS CAMPEON"}
-	document.getElementById("cont1").innerHTML.value = "Pruebassssssss";
-if (document.getElementById("IBSN").value !=""){
-	let x1 = document.getElementById("IBSN").value;
-	alert("CAJA ISBN: " + x1);
-	for(i=0; i<=2; i++) {
-	if(bib[i].ISBN==x1){
-		alert("Encontrado en posición " + i );
-		//document.getElementById("titulo").value=bib[i].titulo;
-		break;
-		//document.getElementById("cont2").innerHTML=bib[i].ISBN;
-		let heading = document.getElementById('cont2');
-		heading.textContent = 'This is a heading';
-	}}}
-else if(document.getElementById("titulo").value != ""){
-	let x1 = document.getElementById("titulo").value;
-	alert("CAJA TITULO: " + x1);
-	for(i=0; i<=2; i++) {
-	if(bib[i].titulo==x1){
-		alert("Encontrado en posición " + i );
-		break;
-		//document.getElementById("cont2").innerHTML=bib[i].ISBN;
-		let heading = document.getElementById('cont2');
-		heading.textContent = 'This is a heading';
-	}}}
-else{
-	alert("No hay dato");
-}
-}
+{	let bib = [{ISBN:"9781234567897",titulo:"CIEN AÑOS DE SOLEDAD",autor:"G. CARCIA M."},{ISBN:"9781234567898",titulo:"LA MARIA",autor:"JORGE ISAACS"},{ISBN:"9781234567899",titulo:"FABULAS",autor:"RAFAEL POMBO"},{ISBN:"9781234567900",titulo:"LA VORAGINE",autor:"JOSE EUTASIO RIVERA"},{ISBN:"9781234567901",titulo:"EL OLVIDO QUE SEREMOS",autor:"HECTOR ABAD"}];
+	var x = 0
+	//alert("DATOS: " + bib[0].ISBN +" "+bib[0].titulo+" "+ bib[0].autor);
+	if (document.getElementById("IBSN").value !=""){
+		let x1 = document.getElementById("IBSN").value;
+		//	alert("CAJA ISBN: " + x1);
+		for(i=0; i<=5; i++) {
+		if(bib[i].ISBN==x1){
+			alert("Hemos encontrado el libro en la posición " + i + ", con el titulo: " + bib[i].titulo + ", y autor: " + bib[i].autor + " por favor solicitelo al bibliotecario");
+			x = 1;
+			alert(x);
+		}}}
+	else if(document.getElementById("titulo").value != ""){
+		let x2 = document.getElementById("titulo").value;
+		//alert("CAJA TITULO: " + x1);
+		for(i=0; i<=5; i++) {
+		if(bib[i].titulo==x2){
+			alert("Hemos encontrado el libro en la posición " + i + ", con el titulo: " + bib[i].titulo + ", y autor: " + bib[i].autor + " por favor solicitelo al bibliotecario");
+			x = 1;
+			alert(x);
+		}}}
+	else if (document.getElementById("titulo").value == "" || document.getElementById("IBSN").value == ""){
+		alert("NO HA DIGITADO ISBN O TITULO");}
+	else if (x!=1){
+			alert("NO TENEMOS EL VOLUMEN DISPONIBLE");
+	}
+	alert(x);
+	}
 
 		
